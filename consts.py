e267e8cd919cae29625acd34b11c2f201f50f3d2
Victor Vieira <victorbor2001@yahoo.com.br>
2023-07-06 20:49:11 -0300

    2.1.3

--- consts.py
+++ consts.py
@@ -1,4 +1,4 @@
-VERSION = "v2.1.2"
+VERSION = "v2.1.3"
 REPO = "https://github.com/victorborneo/D4LP"
 REPO_DIST = "https://github.com/victorborneo/D4LP/tree/main/dist"
 REPO_RELEASES = "https://api.github.com/repos/victorborneo/D4LP/releases/latest"
