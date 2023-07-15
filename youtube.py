9223bf6391b97f98a526af82a048c4c3dc0459a5
Victor Vieira <victorbor2001@yahoo.com.br>
2023-07-15 19:07:13 -0300

    Created print function for threading sync

--- youtube.py
+++ youtube.py
@@ -1,30 +1,41 @@
 import re
+from threading import Lock
 
 from pytube import YouTube, Search
+
+
+print_lock = Lock()
+
+
+def sync_print(*args, **kwargs):
+    with print_lock:
+        print(*args, **kwargs)
 
 
 def download(obj, path):
     name = re.sub(r"\\|\/|\:|\*|\?|\"|\<|\>|\|", "", obj.title)
     name = f"{name}.mp3"
-    print("Downloading", name)
+    sync_print("Downloading", name)
 
     obj.streams.filter(only_audio=True).order_by("abr").desc().first().download(
         filename=name,
         output_path=path
     )
 
+    return name
+
 
 def download_by_link(link, path):
     yt = YouTube(link)
     
-    download(yt, path)
+    return download(yt, path)
 
 
 def download_by_name(name, path):
     s = Search(name)
     yt = s.results[0]
 
-    download(yt, path)
+    return download(yt, path)
 
 
 def main():
